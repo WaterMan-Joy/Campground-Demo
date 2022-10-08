540b81c67320111067d5813a6d28c3acb79bdca6
WaterMan-Joy <nietzsche21st@gmail.com>
2022-10-08 16:46:11 +0900

    add views file (index.js, new.ejs, show.ejs)

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const express = require('express');
+const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const Campground = require('./models/campground')
+
 
 const app = express()
 
@@ -17,10 +19,36 @@ async function main() {
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true}))
+
 app.get('/', (req, res) => {
-    res.send('Home')
+    res.render('home')
 })
 
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index', {
+        campgrounds
+    })
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    // const { id } = req.params
+    const findID = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', {
+        findID
+    })
+})
+
+app.post('/campgrounds', async (req, res) => {
+    res.send(req.body)
+})
+
+
 app.listen(3000, () => {
-    console.log("LISTENING ON PORT 3000")
+    console.log('SERVER ON PORT 3000')
 })
