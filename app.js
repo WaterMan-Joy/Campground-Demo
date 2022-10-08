5fd646030aa57e5294b49eb045e4309aa0b1bc20
WaterMan-Joy <nietzsche21st@gmail.com>
2022-10-08 16:42:04 +0900

    app.js errer catch

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose')
+const path = require('path')
+
 const app = express()
 
+main()
+    .then((res) => {
+        console.log('MONGOOSE CONECT!')
+    })
+    .catch(err => console.log(err));
 
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/testcamp');
+}
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     res.send('Home')
