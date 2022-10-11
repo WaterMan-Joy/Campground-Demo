0a401d27a382d148bd8b528f5bdebbaf1a225221
WaterMan-Joy <nietzsche21st@gmail.com>
2022-10-11 12:50:52 +0900

    add price and description at the show.ejs page

--- seeds/index.js
+++ seeds/index.js
@@ -23,10 +23,13 @@ const seedDB = async () => {
     await Campground.deleteMany({})
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 30) + 100
         const basicCamp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${randomArry(descriptors)}, ${randomArry(places)}`,
-
+            image: 'http://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore natus mollitia iste autem deleniti optio similique labore ipsam nam culpa, quas cum repellat dolorem ab quo sed hic, quibusdam recusandae.',
+            price
         })
         await basicCamp.save()
     }
