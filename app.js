d37b5d0e6f8aad9c40630507499702cf0e0cfc81
WaterMan-Joy <nietzsche21st@gmail.com>
2022-11-08 00:22:22 +0900

    수정

--- app.js
+++ app.js
@@ -30,8 +30,7 @@ const usersRoutes = require("./routes/users");
 
 const app = express();
 
-const dbUrl = "mongodb://localhost:27017/testcamp";
-const myDBUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/testcamp";
 const secret = process.env.SECRET || "isthiswatermankey";
 
 main()
